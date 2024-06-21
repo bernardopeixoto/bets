243f2617ab99572c4d9320b6a658d4e33d04ccff
bernardo <westernhaikus00@gmail.com>
2024-06-21 23:25:41 +0100

    added config to db connection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,17 @@
-import { defineConfig } from "drizzle-kit";
+import { defineConfig, Config } from "drizzle-kit";
+import dotenv from "dotenv";
+dotenv.config();
+
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/schemas",
   out: "./drizzle",
-});
+  dbCredentials: {
+    host: process.env.HOST,
+    port: parseInt(process.env.PORT, 10),
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  },
+}) satisfies Config;
+
